8c6a697774e09c85654b918e59f5472937443f65
Lucas Garron <lgarron@worldcubeassociation.org>
2021-05-11 18:52:49 -0700

    Update CI:
    - Run on all pushes and pull requests.
    - Update `main` branch name.
    - Change the workflow name (and file name).
    - Make CI idempotent (`mkdir-p`).

--- ci-test.sh
+++ ci-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RET=0
-for file in `git diff --name-only thewca/master`; do
+for file in `git diff --name-only origin/main`; do
   if [[ $file == */wca-regulations.md || $file == */wca-guidelines.md ]]; then
     echo "Detected change for file $file, running diff."
     wrc $file --diff wca-regulations-official
@@ -10,7 +10,7 @@ done
 
 LANGUAGES=`wrc-languages`
 echo "================================="
-mkdir "build"
+mkdir -p "build"
 for l in $LANGUAGES; do
   INPUTDIR=${l}
   echo "Doing check for language "${l}
